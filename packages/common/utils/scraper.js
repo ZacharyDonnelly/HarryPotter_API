b95a192f93d8bd4ce33303d5ae5cfa29e547832a
parxyHash <z-donnelly@hvcc.edu>
2020-04-19 23:17:49 -0400

    setup scraper for character names

---
+++ packages/common/utils/scraper.js
@@ -0,0 +1,8 @@
+const scraper = require('./scraper-hook');
+
+const slytherin = 'https://harrypotter.fandom.com/wiki/Category:Slytherins';
+const gryffindor = 'https://harrypotter.fandom.com/wiki/Category:Gryffindors';
+const ravenclaw = 'https://harrypotter.fandom.com/wiki/Category:Ravenclaws';
+const hufflepuff = 'https://harrypotter.fandom.com/wiki/Category:Hufflepuffs';
+
+scraper(hufflepuff);
