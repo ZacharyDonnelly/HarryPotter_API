05ced4d265de3ed1f1c7c8f265c3e79aa51084d3
parxyHash <z-donnelly@hvcc.edu>
2020-04-20 00:30:05 -0400

    setup scraper awaiting for db to input character names

--- packages/common/utils/scraper.js
+++ packages/common/utils/scraper.js
@@ -1,8 +1,39 @@
-const scraper = require('./scraper-hook');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const slytherin = 'https://harrypotter.fandom.com/wiki/Category:Slytherins';
-const gryffindor = 'https://harrypotter.fandom.com/wiki/Category:Gryffindors';
-const ravenclaw = 'https://harrypotter.fandom.com/wiki/Category:Ravenclaws';
-const hufflepuff = 'https://harrypotter.fandom.com/wiki/Category:Hufflepuffs';
+const scraper = (url) => {
+  axios
+    .get(url)
+    .then((res) => {
+      const html = res.data;
+      const $ = cheerio.load(html);
+      const charTable = $(
+        '.category-page__members > .category-page__members-for-char > .category-page__member'
+      );
+      const allCharacters = [];
 
-scraper(hufflepuff);
+      charTable.each(function () {
+        const Character = $(this).find('.category-page__member-link').text();
+        allCharacters.push({ Character });
+      });
+      return allCharacters;
+    })
+    .then((res) => {
+      const arr = [];
+      arr.push(res);
+      console.log(arr);
+      return arr;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const slytherinURL = 'https://harrypotter.fandom.com/wiki/Category:Slytherins';
+const gryffindorURL = 'https://harrypotter.fandom.com/wiki/Category:Gryffindors';
+const ravenclawURL = 'https://harrypotter.fandom.com/wiki/Category:Ravenclaws';
+const hufflepuffURL = 'https://harrypotter.fandom.com/wiki/Category:Hufflepuffs';
+
+scraper(hufflepuffURL);
+
+module.exports = scraper;
